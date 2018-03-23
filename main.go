b5ee0986d414e98eafa75f03f9e5e84e3c3ee993
Dan Sosedoff <dan.sosedoff@gmail.com>
2018-03-22 22:29:47 -0500

    Initial

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/grandcat/zeroconf"
+)
+
+var (
+	name     string
+	protocol string
+	domain   string
+	port     int
+)
+
+func init() {
+	flag.StringVar(&name, "name", "", "Service name")
+	flag.StringVar(&protocol, "protocol", "_http._tcp", "Service protocol")
+	flag.StringVar(&domain, "domain", "local.", "Service domain")
+	flag.IntVar(&port, "port", 80, "Service port")
+	flag.Parse()
+
+	if name == "" {
+		hostname, err := os.Hostname()
+		if err != nil {
+			log.Println("cant get hostname:", err)
+		}
+		if hostname == "" {
+			log.Fatal("host is not set")
+		}
+		name = hostname
+	}
+}
+
+func main() {
+	log.Printf(
+		"starting zeroconf service name=%v protocol=%v domain=%v port=%v\n",
+		name, protocol, domain, port,
+	)
+	defer log.Println("stopping zeroconf service")
+
+	server, err := zeroconf.Register(name, protocol, domain, port, []string{}, nil)
+	if err != nil {
+		log.Fatal("zeroconf server error:", err)
+	}
+	defer server.Shutdown()
+
+	stop := make(chan bool)
+	<-stop
+}
